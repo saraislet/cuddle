46cd3dfa6f08c17df059806bfbdee3d736e570f0
Sarai Rosenberg <srosenberg@pagerduty.com>
2018-05-15 09:48:48 -0700

    write cuddle function handle subdomain and curl with variables treat www as nil

--- cuddle.py
+++ cuddle.py
@@ -8,19 +8,33 @@ browser_response = "Cuddles!"
 @app.route("/")
 def main():
     """Return cuddles."""
-    if request.headers.get('User-Agent').startswith("curl"):
+    if request.headers.get("User-Agent").startswith("curl"):
         return curl_response
     
     return browser_response
 
 @app.route("/<string:sub>")
-def cuddle(sub):
+def handle_sub(sub):
     """Return cuddles from sub."""
-    if request.headers.get('User-Agent').startswith("curl"):
-        return "{} curls up in your cuddles.".format(sub)
-    
-    return "{} cuddles you.".format(sub)
+    curl = False
 
+    if request.headers.get("User-Agent").startswith("curl"):
+        curl = True
+
+    if sub == "www":
+        return cuddle(None, curl)
+    return cuddle(sub, curl)
+
+def cuddle(sub, curl):
+    """Given string and boolean, return cuddle string."""
+    if not sub:
+        if not curl:
+            return browser_response
+        return curl_response
+    
+    if not curl:
+        return "{} cuddles back.".format(sub)
+    return "{} curls up in your cuddles.".format(sub)
 
 if __name__ == "__main__":
     app.run()
