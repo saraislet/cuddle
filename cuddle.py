8d36f80471bb03a95614b1ad2e1249d459a42b7c
Sarai Rosenberg <srosenberg@pagerduty.com>
2018-05-14 21:22:20 -0700

    add curl guardian

--- cuddle.py
+++ cuddle.py
@@ -1,15 +1,17 @@
 import requests
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 curl_response = "Your cuddle partner curls back."
-
+browser_response = "Cuddles!"
 
 @app.route("/")
 def main():
     """Return cuddles."""
-    return curl_response
-
+    if request.headers.get('User-Agent').startswith("curl"):
+        return curl_response
+    
+    return browser_response
 
 if __name__ == "__main__":
     app.run()
